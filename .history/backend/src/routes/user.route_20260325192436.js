619d8ce08174732750dd98bc5f0ef63943264e4b
Goddydev1 <148202566+gogg23@users.noreply.github.com>
2026-03-25 20:25:54 +0300

    Your commit message here

--- .history/backend/src/routes/user.route_20260325192436.js
+++ .history/backend/src/routes/user.route_20260325192436.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
 import { loginUser, registerUser } from '../controllers/user.controller.js';
-
 const router = Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
 
 export default router;
